a76b4864e3a261d9c375b41a867d7bbe07a543c3
FBarbossa <franciscosbarbosas@gmail.com>
2026-02-09 18:48:08 +0000

    fix: remove dropdown arrows from navigation menu

--- components/HeaderItem.jsx
+++ components/HeaderItem.jsx
@@ -60,7 +60,7 @@ const HeaderItem = ({ item, mobile, onSubmenuShow, onSubmenuHide }) => {
     if (item.hasSubmenu) {
         return (
             <div
-                className="relative cursor-default hover:underline lg:pr-4 menu-with-submenu inline-block"
+                className="relative cursor-default hover:underline menu-with-submenu inline-block"
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
                 onClick={() => {
@@ -76,7 +76,7 @@ const HeaderItem = ({ item, mobile, onSubmenuShow, onSubmenuHide }) => {
                             ? "underline mix-blend-difference"
                             : "mix-blend-difference",
                     )}>
-                    {item.label} <ArrowDown flip={isOpen} />
+                    {item.label}
                 </h3>
                 <div
                     ref={submenuRef}
@@ -103,20 +103,5 @@ const HeaderItem = ({ item, mobile, onSubmenuShow, onSubmenuHide }) => {
         )
     }
 }
-
-const ArrowDown = ({ flip }) => (
-    <motion.svg
-        viewBox="0 0 67 77"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className="origin-center w-2 ml-2 fill-current block absolute left-full top-1/2 -translate-y-1/2">
-        <motion.path
-            d="M66.585 43.5351L58.255 35.2051L39.605 53.8551L39.605 0.965087L27.825 0.965086L27.825 53.8551L9.17496 35.2051L0.844954 43.5351L33.715 76.3951L66.585 43.5351Z"
-            style={{ originX: 0.5, originY: 0.5 }}
-            animate={{ rotate: flip ? -180 : 0 }}
-            transition={{ duration: 0.25 }}
-        />
-    </motion.svg>
-)
 
 export default HeaderItem
