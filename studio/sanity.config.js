9823d54cb33280a347e1efc33bf05ca34d289bdd
FBarbossa <franciscosbarbosas@gmail.com>
2026-02-09 17:44:31 +0000

    Update sanity.config.js

--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -2,7 +2,7 @@ import { visionTool } from "@sanity/vision"
 import { defineConfig } from "sanity"
 import { media } from "sanity-plugin-media"
 import { structureTool } from "sanity/structure"
-import deskStructure from "./desk-structure"
+import deskStructure, { getDefaultDocumentNode } from "./desk-structure"
 import documents from "./schemas/documents"
 import objects from "./schemas/objects"
 
@@ -13,6 +13,7 @@ export default defineConfig({
     plugins: [
         structureTool({
             structure: deskStructure,
+            defaultDocumentNode: getDefaultDocumentNode,
         }),
         visionTool(),
         media(),
